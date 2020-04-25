947ca82fe952707face1bf86f1adfa294902a53a
shreyansbardia <shreyansbardia62@gmail.com>
2020-04-25 21:30:16 +0530

    Add files via upload

---
+++ WhatsappAutomation.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 21:28:39 2020
+
+@author: ADMIN
+"""
+
+from selenium import webdriver
+#Connect with the ChromeDriver
+driver = webdriver.Chrome(executable_path='C:\chromedriver_win32\chromedriver.exe')
+
+#Go to the Website
+driver.get('https://web.whatsapp.com/')
+
+
+name = "demo" #Name of the person to send
+msg = "Hi" #Message to be send
+count = 4 #Number of times message to be sent
+
+input('Enter anything after scanning QR code')
+
+#Finding the name of the person via XPATH - To get the Xpath Inspect the element and click copy -copy Xpath
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click() #Click on the user
+
+msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]') # fIND THE TEXTbox
+
+for i in range(count):
+    msg_box.send_keys(msg) # Message that should be sent
+    button = driver.find_element_by_class_name('_35EW6') #Find send button via Xpath
+    button.click() #Click on send button
